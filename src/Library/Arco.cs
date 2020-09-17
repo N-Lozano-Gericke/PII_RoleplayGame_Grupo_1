606d309ec74c9018cca37974e42961ab52e801f3
MartinPerciante <69222963+MartinPerciante@users.noreply.github.com>
2020-09-17 17:53:20 -0300

    Martin Perciante Elfo + Comentarios y Tests

--- src/Library/Arco.cs
+++ src/Library/Arco.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Program
 {
+    // La clase Arco esta separada de la clase Elfo, ya que la Arco es un objeto que el Elfo puede utilizar pero que
+    // no es propio de este. Además, según el patron SRP la clase Elfo no debería encargarse de los objetos que puedan
+    // ser utilizados por el Elfo. Ademas, por el patron EXPERT, es la Arco quien debe encargarse de conocer sus atributos.
     public class Arco
     {
         public int ataque;
--- src/Library/Daga.cs
+++ src/Library/Daga.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Program
 {
+    // La clase Daga esta separada de la clase Elfo, ya que la Daga es un objeto que el Elfo puede utilizar pero que
+    // no es propio de este. Además, según el patron SRP la clase Elfo no debería encargarse de los objetos que puedan
+    // ser utilizados por el Elfo. Ademas, por el patron EXPERT, es la Daga quien debe encargarse de conocer sus atributos.
     public class Daga
     {
         public int ataque;
